151c5ea283e4d690451c34c3ef8dfeaaf041e99e
DooongJae <rlaehdwosla2@gmail.com>
2024-10-04 11:57:21 +0900

    동재 수정
    1. 종료 버튼 누를 시 기록 파일 삭제
    2. 사용자가 입력 창에 다시 입력하면 파일 생성과 동시에 사용자, gpt 내용 기록 진행

--- main.py
+++ main.py
@@ -10,15 +10,17 @@ load_dotenv()
 app = Flask(__name__)
 
 # OpenAI API 키 설정 (환경 변수에서 가져오기)
-openai.api_key = os.getenv('FLASK_API_KEY')
+openai.api_key = 'Key'
+
+file_path = 'responses.txt'
 
 # 이전 응답과 캐릭터 정보를 파일에서 읽어오는 함수
 def read_previous_responses():
     responses = ''
     rules = ''
     
-    if os.path.exists('responses.txt'):
-        with open('responses.txt', 'r', encoding='utf-8') as file:
+    if os.path.exists(file_path):
+        with open(file_path, 'r', encoding='utf-8') as file:
             responses = file.read().strip()
     
     if os.path.exists('rules.txt'):
@@ -26,6 +28,16 @@ def read_previous_responses():
             rules = file.read().strip()
 
     return responses, rules
+
+# responses.txt 파일을 생성
+def write_to_responses(user_input, gpt_response):
+    with open('responses.txt', 'a', encoding='utf-8') as responses_file:
+        responses_file.write(f"user: {user_input}\n")
+        responses_file.write(f"host: {gpt_response}\n\n")
+
+# 랜덤 다이스 2개를 굴려줄 함수.
+def roll_dice():
+    return random.randint(1, 6) + random.randint(1, 6)
 
 @app.route('/')
 def start():
@@ -49,6 +61,11 @@ def call_gpt():
         
         # 이전 응답 및 규칙 정보 읽기
         previous_responses, rules = read_previous_responses()
+        
+        # "TRPG 마치기" 입력 처리
+        if prompt.strip().lower() == "trpg 마치기" and os.path.exists(file_path):
+            os.remove(file_path)  # response.txt 파일 삭제
+            return jsonify({"response": "WorldLog TRPG를 마치겠습니다..! 재밌게 시간 보내주셔서 감사합니다!"})
 
         # GPT-4 모델 호출
         response = openai.ChatCompletion.create(
@@ -66,13 +83,23 @@ def call_gpt():
         generated_text = response.choices[0].message['content'].strip()
 
         # 응답 내용을 UTF-8 인코딩으로 파일에 저장
-        with open('responses.txt', 'a', encoding='utf-8') as file:
-            file.write(f"Prompt: {prompt}\nResponse: {generated_text}\n\n")
-
+        write_to_responses(prompt, generated_text)
+       
         return jsonify({"response": generated_text})
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+    
+    
+@app.route('/end-trpg', methods=['POST'])
+def end_trpg():
+    file_path = "responses.txt"
+
+    if os.path.exists(file_path):
+        os.remove(file_path)  # history.txt 파일 삭제
+        return jsonify({"response": "WorldLog TRPG를 마치겠습니다..! 재밌게 시간 보내주셔서 감사합니다!"})
+    
+    return jsonify({"response": "responses.txt 파일이 존재하지 않습니다."})
 
 # Flask 애플리케이션 실행
 if __name__ == '__main__':
